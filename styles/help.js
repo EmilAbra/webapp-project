f2fd1f57bf63c9bf65f61f830a35b0984ed91d0f
eabra <emab21@student.bth.se>
2022-11-21 09:04:18 +0100

    add help page

---
+++ styles/help.js
@@ -0,0 +1,17 @@
+// export const flex = {
+//     flex: 1,
+// };
+
+export const textContainer = {
+    backgroundColor: '#fff',
+    height: 'auto',
+    marginLeft: 20,
+    marginRight: 20,
+    top: 200,
+    borderRadius: 5,
+    elevation: 5,
+    padding: 20,
+    paddingBottom: 0
+    // alignItems: 'center',
+    // justifyContent: 'center',
+};
