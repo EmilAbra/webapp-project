de444f418ea619cb886f6a4a39e78b86cddf7514
eabra <emab21@student.bth.se>
2022-11-21 09:21:54 +0100

    add user functionality

--- models/storage.ts
+++ models/storage.ts
@@ -18,6 +18,8 @@ const storage = {
     readToken: async function readToken(): Promise<any> {
         try {
             const jsonValue = await AsyncStorage.getItem('@token');
+            // console.log(jsonValue);
+            
             return jsonValue != null ? JSON.parse(jsonValue) : null;
         } catch (e) {
             console.log(e);
