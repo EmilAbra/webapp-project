c1b9fe35e8c0c1971bd108afdbf18f60b675a90c
eabra <emab21@student.bth.se>
2022-12-03 09:02:08 +0100

    add new font-family

--- styles/typography.js
+++ styles/typography.js
@@ -1,30 +1,55 @@
 export const header1 = {
     fontSize: 42,
+    fontFamily: 'sjsans_regular',
     textAlign: 'center',
     marginBottom: 15
 };
 
 export const header2 = {
     fontSize: 34,
-    marginBottom: 28,
-    paddingLeft: 12
+    marginBottom: 20,
+    paddingLeft: 12,
+    fontFamily: 'sjsans_bold',
+    textAlign: 'center',
+};
+
+export const header2Home = {
+    fontFamily: 'sjsans_bold',
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 34,
+    marginBottom: 5,
 };
 
 export const header3 = {
+    fontFamily: 'sjsans_bold',
     fontSize: 28,
     marginBottom: 28,
     paddingLeft: 12
 };
 
 export const header4 = {
+    fontFamily: 'sjsans_bold',
     fontSize: 24,
     padding: 12,
-    paddingTop: 20,
+    paddingTop: 40,
+    paddingLeft: 15,
     marginBottom: 20,
 };
 
 export const normal = {
+    fontFamily: 'sjsans_regular',
+    fontSize: 18,
+    marginBottom: 28,
+    color: '#444',
+    paddingLeft: 12,
+    paddingRight: 12
+};
+
+export const normalBold = {
+    fontFamily: 'sjsans_regular',
     fontSize: 20,
+    marginTop: 20,
     marginBottom: 28,
     color: '#444',
     paddingLeft: 12,
@@ -32,10 +57,42 @@ export const normal = {
 };
 
 export const normalCentre = {
-    fontSize: 18,
+    fontFamily: 'sjsans_regular',
+    fontSize: 16,
     marginBottom: 28,
     color: '#444',
     paddingLeft: 15,
     paddingRight: 15,
     textAlign: 'center'
+};
+
+export const stationButtonText = {
+    fontFamily: 'sjsans_regular',
+    fontSize: 17,
+    color: "#000",
+};
+
+export const stationDetailsText = {
+    fontFamily: 'sjsans_regular',
+    fontSize: 14,
+    marginBottom: 20,
+    color: '#444',
+    paddingLeft: 12,
+    paddingRight: 12
+};
+
+export const loginButtonTitle = {
+    fontSize: 20,
+    fontFamily: 'sjsans_bold',
+    color: '#FFF', 
+    textAlign: 'center',
+    padding: 4
+};
+
+export const registerButtonTitle = {
+    fontSize: 17,
+    fontFamily: 'sjsans_regular',
+    color: '#212427',
+    textAlign: 'center',
+    padding: 4
 };
